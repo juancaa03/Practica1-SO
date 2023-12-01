9655e8d9a5ad4a89476136874dba73e3ffa95ec2
Juan Carlos Medinilla Alonso <juancarlos.medinilla@estudiants.urv.cat>
2023-12-01 09:51:20 +0100

    script sql

---
+++ Homework1/test/resources/sql/bdso.sql
@@ -0,0 +1,13 @@
+INSERT INTO root.VIDEOJOC (id, nom, videoconsola, disponibilitat, preuLloguer, descripcio, tipus, adrecaBotigues)
+VALUES 
+  (10, 'Super Mario Land', 'Game Boy', 1, 10.99, 'Clásico juego de plataformas', 'Plataforma', 'The Retro Games Store, pl. del Blat 3, Valls, 43800'),
+  (11, 'Quake', 'PC', 0, 15.99, 'Juego de acción en primera persona', 'Acción', 'Game Haven, Calle de Ejemplo 123, Ciudad, 12345'),
+  (12, 'The Legend of Zelda', 'Nintendo Entertainment System', 1, 12.99, 'Aventura épica', 'Aventura', 'Gamer Paradise, Av. Principal 456, Pueblo, 56789');
+
+INSERT INTO root.Usuari (id, nomUsuari, contrasenya, correu)
+VALUES
+    (100, 'MarcJimenez', 'marc123', 'marcj16@gmail.com'),
+    (101, 'CarlosEscrit', 'carlos123', 'carlos12@gmail.com'),
+    (102, 'JuancaAlonso', 'juanca123', 'juanca8@gmail.com'),
+    (103, 'AbrilGuzman', 'abril123', 'abril8@gmail.com'),
+    (104, 'CocoIbbie', 'coco123', 'coco8@gmail.com');
